4e80147ba13ceadb4de86d05eab33d792d705efe
iizhukov <iizhukov@icloud.com>
2024-09-06 00:07:41 +0300

    ADD: Fibonacci class, startup Program class and tests

---
+++ FibonacciLib.Tests/UnitTest1.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using FibonacciLib;
+using NUnit.Framework.Legacy;
+
+namespace FibonacciLib.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void NIsEqualTo0()
+    {
+        int[] excepted = [];
+        var result = Fibonacci.Fib(0);
+
+        CollectionAssert.AreEqual(excepted, result);
+    }
+
+    [Test]
+    public void NIsEqualTo1()
+    {
+        int[] excepted = [0];
+        var result = Fibonacci.Fib(1);
+
+        CollectionAssert.AreEqual(excepted, result);
+    }
+
+    [Test]
+    public void NIsEqualTo6()
+    {
+        int[] excepted = [0, 1, 1, 2, 3, 5];
+        var result = Fibonacci.Fib(6);
+
+        CollectionAssert.AreEqual(excepted, result);
+    }
+
+    [Test]
+    public void NIsEqualTo10()
+    {
+        int[] excepted = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+        var result = Fibonacci.Fib(10);
+
+        CollectionAssert.AreEqual(excepted, result);
+    }
+}
---
+++ first-lesson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FibonacciLib;
+
+namespace first_lesson {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Введите длину последовательности Фибоначчи: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] fib = Fibonacci.Fib(n);
+
+            for (int i = 0; i < n - 1; ++i) {
+                Console.Write($"{fib[i]}, ");
+            }
+            Console.WriteLine($"{fib[n - 1]}");
+
+            Console.ReadKey();
+        }
+    }
+}
